9ef399c18cbb9be5c72a69f1b38876c6d6346b3e
mkc1000 <mkc1000@gmail.com>
2016-12-20 11:21:41 -0800

    Update within_cluster_variance.py

--- within_cluster_variance.py
+++ within_cluster_variance.py
@@ -4,17 +4,24 @@ import random
 from datetime import datetime
 
 def mean_cluster_variances(clusters, feature):
+    """Inputs:
+        clusters: 1D numpy array of cluster IDs, shape (n)
+        feature: 1D numpy array values for a given feature, shape (n)
+    """
     unique_clusters = np.unique(clusters)
     variances = []
     sizes = []
     for cluster in unique_clusters:
-        cluster_feature = feature[clusters==cluster]
+        cluster_feature = feature[clusters == cluster]
         variances.append(np.var(cluster_feature))
         sizes.append(len(cluster_feature))
-    return np.array(variances).dot(np.array(sizes))/len(feature)
+    weighted_variances = np.array(variances).dot(np.array(sizes)) / len(feature)
+    return weighted_variances
 
 def score_once(wcv, data, i):
-    y = data[:,i]
+    """[docstring]
+    """
+    y = data[:, i]
     X = np.delete(data, i, axis=1)
     predictions = wcv.model.fit_predict(X)
     n_clusters = len(np.unique(predictions))
@@ -24,6 +31,8 @@ def score_once(wcv, data, i):
     return scaled_within_cluster_variance, n_clusters
 
 class WCVScore(object):
+    """[docstring]
+    """
     def __init__(self, model, max_iter=10, n_jobs=1):
         self.model = model
         self.wcvs = []
@@ -36,9 +45,9 @@ class WCVScore(object):
         n_features = data.shape[1]
         features = np.arange(n_features)
         if n_features > self.max_iter:
-            features = np.random.choice(features,size=self.max_iter,replace=False)
+            features = np.random.choice(features, size=self.max_iter, replace=False)
         output = Parallel(n_jobs=self.n_jobs)(delayed(score_once)(self, data, i) for i in features)
         output = np.array(output)
-        self.wcvs = output[:,0]
-        self.n_clusters = output[:,1]
+        self.wcvs = output[:, 0]
+        self.n_clusters = output[:, 1]
         return np.mean(self.wcvs), np.mean(self.n_clusters)
