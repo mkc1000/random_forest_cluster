20e8e83f558e4edf714a503974d062578539ef65
mkc1000 <mkc1000@gmail.com>
2016-12-20 14:15:54 -0800

    Update within_cluster_variance.py

--- within_cluster_variance.py
+++ within_cluster_variance.py
@@ -5,8 +5,10 @@ from datetime import datetime
 
 def mean_cluster_variances(clusters, feature):
     """Inputs:
-        clusters: 1D numpy array of cluster IDs, shape (n)
-        feature: 1D numpy array values for a given feature, shape (n)
+         clusters: 1D numpy array of cluster IDs, shape (n)
+         feature: 1D numpy array values for a given feature, shape (n)
+        
+       Output: Float; the weighted average across all clusters of the within-cluster-variance of the input feature
     """
     unique_clusters = np.unique(clusters)
     variances = []
@@ -15,11 +17,18 @@ def mean_cluster_variances(clusters, feature):
         cluster_feature = feature[clusters == cluster]
         variances.append(np.var(cluster_feature))
         sizes.append(len(cluster_feature))
-    weighted_variances = np.array(variances).dot(np.array(sizes)) / len(feature)
-    return weighted_variances
+    weighted_average_variance = np.array(variances).dot(np.array(sizes)) / len(feature)
+    return weighted_average_variance
 
 def score_once(wcv, data, i):
-    """[docstring]
+    """Effectively a method to the object below, but pulled outside so it could parallelized more easily.
+    Inputs:
+      wcv: A WCVScore model; acts just like "self"
+      data: 2D numpy array
+      i: Int; column index for data
+      
+    Output: Float; the within-cluster-variance score only for feature i,
+            given cluster assignments that are generated only using the remaining features
     """
     y = data[:, i]
     X = np.delete(data, i, axis=1)
@@ -31,7 +40,20 @@ def score_once(wcv, data, i):
     return scaled_within_cluster_variance, n_clusters
 
 class WCVScore(object):
-    """[docstring]
+    """
+    Arguments:
+      model: a clustering model with a fit_predict method. That fit_predict method must take a 2D numpy array, shape (n, k)
+             and return a 1D numpy array of cluster IDs, shape (n)
+      max_iter: Features are withheld one by one, and self.model.fit_predict is called each time.
+                If max_iter < k, this will only be done for a random subset of the features.
+      n_jobs: Allows parallelization of the loop described above.
+      
+    Methods:
+      score:
+        Inputs:
+          data: 2D numpy array
+        Output: (cluster validation score for the algorithm implemented in self.model,
+                 average number of clusters identified by self.model)
     """
     def __init__(self, model, max_iter=10, n_jobs=1):
         self.model = model
